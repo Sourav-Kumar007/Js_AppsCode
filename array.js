f160e0662aea9175bab46f7e01047f121ff85d95
Sourav Roy <souravkumarroy813965@gmail.com>
2025-02-11 18:47:44 +0600

    some files added

---
+++ array.js
@@ -0,0 +1,49 @@
+
+const fruits = [];
+fruits.push('hello');
+console.log(fruits); 
+fruits.push(5);
+for(let x in fruits)
+{
+   // console.log(fruits[x]);
+}
+
+          // array iteration 
+
+          // Array.forEach 
+
+const numbers = [45 , 4 , 9 , 16 , 25];
+
+
+function myFun(value , index , array)
+{
+    console.log(value);
+    console.log(index);
+}
+
+numbers.forEach(myFun);
+
+const number1 = [45,4,9,16,25];
+const number2 = number1.map(myFun1);
+
+function myFun1(value , index , array)
+{
+    return value * 2;
+}
+console.log(number2); 
+
+const over18 = numbers.filter(myFun2);
+
+function myFun2(value){
+    return value > 18;
+}
+console.log(over18);
+
+
+let sum = numbers.reduce(myFun3);
+
+function myFun3(total, value , index, array)
+{
+    return total + value;
+}
+console.log(sum);
---
+++ math.js
@@ -0,0 +1,8 @@
+
+console.log(Math.PI);
+
+console.log(Math.random() * 10);
+
+console.log(Math.ceil(Math.random() * 100));
+
+
