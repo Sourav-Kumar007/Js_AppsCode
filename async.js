2fdcda813edf13fbb3a46cc4e14ccc66c0dff3f3
Sourav Roy <souravkumarroy813965@gmail.com>
2025-02-13 18:44:42 +0600

    promise added

---
+++ async.js
@@ -0,0 +1,24 @@
+
+console.log('Line 1 code');
+
+console.log('Line 2 code');
+
+setTimeout(function(){
+    console.log('Line shuru');
+} , 5000);
+
+console.log('Line 3 code');
+console.log('Line 4 code');
+console.log('Line 5 code');
+
+setTimeout(function(){
+    console.log('Line sesh');
+} , 1000);
+
+console.log('Line 6 code');
+console.log('Line 7 code');
+console.log('Line 8 code');
+console.log('Line 9 code');
+console.log('Line 10 code');
+console.log('Line 11 code');
+console.log('Line 12 code');
